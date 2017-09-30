538adbc6e26767c90c3c763dbd7d026aede8d066
Nam Nguyen <namnh2711@gmail.com>
2017-09-30 23:52:37 +0700

    pg

---
+++ server/db.js
@@ -0,0 +1,18 @@
+const { Pool } = require('pg')
+;
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'pass',
+  port: 5432,
+});
+
+pool.connect();
+
+pool.query('SELECT * from books', [], (err, res) => {
+  console.log(err ? err.stack : res.rows); // Hello World!
+  pool.end();
+})
+;
